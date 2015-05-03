9f3a5f791f4d6478da37360c80db3f811c2ab276
alvarogoher <alvarogoher@hotmail.com>
2015-05-03 20:09:30 +0200

    Página de créditos

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,14 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET /author */
+router.get('/author', function(req, res, next) {
+    res.render('author', {
+        name   : 'Álvaro Gómez Hernández',
+        picture: '<img src="/images/ALVARO2.GIF" width="100px" alt="Álvaro Gómez">'
+    });
+});
+
 /* GET /quizes/question */ 
 router.get('/quizes/question', quizController.question);
 
