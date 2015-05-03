8beb7fb61d1ce07ac4178363c45c8d4ddff93222
alvarogoher <alvarogoher@hotmail.com>
2015-05-03 19:47:45 +0200

    CSS adaptable a móviles

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,23 @@ var router = express.Router();
  
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+/* GET home page */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
- 
+
+/* GET /author */
+router.get('/author', function(req, res, next) {
+    res.render('author', {
+        name   : 'Álvaro Gómez Hernández',
+        picture: '<img src="/images/ALVARO2.gif" width="100px" alt="Mikel Goig">'
+    });
+});
+
+/* GET /quizes/question */ 
 router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer',   quizController.answer);
 
+/* GET /quizes/answer */
+router.get('/quizes/answer',   quizController.answer)
 
- module.exports = router;
+module.exports = router;
