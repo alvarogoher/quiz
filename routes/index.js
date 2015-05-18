3b082b03de540516d737f3b32d6d49239313d872
alvarogoher <alvarogoher@hotmail.com>
2015-05-18 21:23:02 +0200

    Moderación de Comentarios

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,8 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('commentId', commentController.load);  // autoload :commentId
+
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -45,5 +47,7 @@ router.get('/author', function(req, res, next) {
 // Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
- 
- module.exports = router;
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
+	                                    sessionController.loginRequired, commentController.publish);
+
+module.exports = router; 
