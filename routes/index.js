a93c8f6e66c19a2b8833228c778c09cea3298d2d
alvarogoher <alvarogoher@hotmail.com>
2015-05-19 15:48:11 +0200

    Página de estadísticas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
  
-var quizController = require('../controllers/quiz_controller');
-var commentController = require('../controllers/comment_controller');
-var sessionController = require('../controllers/session_controller');
+var quizController       = require('../controllers/quiz_controller');
+var commentController    = require('../controllers/comment_controller');
+var sessionController    = require('../controllers/session_controller');
+var statisticsController = require('../controllers/statistics_controller');
 
 
 // Página de entrada (home page)
@@ -34,20 +35,24 @@ router.get('/author', function(req, res, next) {
 });
 
 
- // Definición de rutas de /quizes
- router.get('/quizes',                      quizController.index);
- router.get('/quizes/:quizId(\\d+)',        quizController.show);
- router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
- +router.get('/quizes/new', 				   sessionController.loginRequired, quizController.new);
-+router.post('/quizes/create',              sessionController.loginRequired, quizController.create);
-+router.get('/quizes/:quizId(\\d+)/edit',   sessionController.loginRequired, quizController.edit);
-+router.put('/quizes/:quizId(\\d+)',        sessionController.loginRequired, quizController.update);
-+router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quizController.destroy);
+/* Definición de rutas de /quizes */
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',                  sessionController.loginRequired, quizController.new);
+router.post('/quizes/create',              sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',   sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)',        sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quizController.destroy);
+
  
 // Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
 router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
 	                                    sessionController.loginRequired, commentController.publish);
+
+/* Definición de rutas de estadísticas */
+router.get('/quizes/statistics', statisticsController.statistics);
 
 module.exports = router; 
