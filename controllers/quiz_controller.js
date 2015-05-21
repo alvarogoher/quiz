7319c03e8a7446daa8d01172dfbbe308bba108b1
alvarogoher <alvarogoher@hotmail.com>
2015-05-22 00:08:27 +0200

    Tabla de usuarios

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -82,21 +82,21 @@ exports.new = function(req, res) {
 
 // POST /quizes/create
 exports.create = function(req, res) {
- var quiz = models.Quiz.build( req.body.quiz );
-
- quiz
- .validate()
- .then(
-   function(err){
-     if (err) {
-       res.render('quizes/new', {quiz: quiz, errors: err.errors});
-     } else {
-       quiz // save: guarda en DB campos pregunta y respuesta de quiz
-       .save({fields: ["pregunta", "respuesta"]})
-       .then( function(){ res.redirect('/quizes')}) 
-     }      // res.redirect: Redirección HTTP a lista de preguntas
-   }
-).catch(function(error){next(error)});
+  req.body.quiz.UserId = req.session.user.id;
+  var quiz = models.Quiz.build( req.body.quiz );
+  quiz
+  .validate()
+  .then(
+  function(err){
+    if (err) {
+      res.render('quizes/new', {quiz: quiz, errors: err.errors});
+    } else {
+      quiz // save: guarda en DB campos pregunta y respuesta de quiz
+      .save({fields: ["pregunta", "respuesta", "UserId"]})
+      .then( function(){ res.redirect('/quizes')}) 
+    }      // res.redirect: Redirección HTTP a lista de preguntas
+  }
+  ).catch(function(error){next(error)});
 };
 
 // GET /quizes/:id/edit
